ed1ebef97f14b34b725784fc31f806b6a475c802
Mostafa Ahangarha <ahangarha@riseup.net>
2022-05-19 13:47:16 +0300

    update factorial for positive numbers

--- solver.rb
+++ solver.rb
@@ -2,6 +2,7 @@ class Solver
 
   def factorial(number)
     raise 'Negative Number not allowed!' unless number >= 0
-    return 1
+    return 1 if number == 0 || number == 1
+    return number * factorial(number - 1)
   end
 end
