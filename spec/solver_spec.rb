6115eeeab7aa351db49d7c29a6b4f415a6fe1cc9
Mostafa Ahangarha <ahangarha@riseup.net>
2022-05-19 13:33:19 +0300

    add test for factorial with negative number

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -11,5 +11,9 @@ describe 'Solver' do
     it 'should return 1 when N = 0' do
       expect(Solver.new.factorial(0)).to eq(1)
     end
+
+    it 'should raise exception on negative input' do
+      expect { Solver.new.factorial(-5) }.to raise_error('Negative Number not allowed!')
+    end
   end
 end
