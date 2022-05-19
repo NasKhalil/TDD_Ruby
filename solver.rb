80dc3b90ed469af952795eb4b4bcbc562f61946f
Mostafa Ahangarha <ahangarha@riseup.net>
2022-05-19 17:27:54 +0300

    change methods to static

--- solver.rb
+++ solver.rb
@@ -1,16 +1,16 @@
 class Solver
-  def factorial(number)
+  def self.factorial(number)
     raise 'Negative Number not allowed!' unless number >= 0
     return 1 if [0, 1].include?(number)
 
     number * factorial(number - 1)
   end
 
-  def reverse(str)
+  def self.reverse(str)
     str.reverse
   end
 
-  def fizzbuzz(number)
+  def self.fizzbuzz(number)
     return 'fizzbuzz' if (number % 3).zero? && (number % 5).zero?
     return 'fizz' if (number % 3).zero?
     return 'buzz' if (number % 5).zero?
