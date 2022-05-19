2d241f73093f8650d86365f754ebb27b4fd071b9
Khalil <khalilnasri92@gmail.com>
2022-05-19 11:12:58 +0100

    add test for factorial of 0

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -6,4 +6,10 @@ describe 'Solver' do
       expect(Solver.is_a?(Class)).to be true
     end
   end
+
+  context('factorial method') do
+    it 'should return 1 when N = 0' do
+      expect(Solver.new.factorial(0)).to eq(1)
+    end
+  end
 end
